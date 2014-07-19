8ba3cc5bc501b573e32e9f3951f054a312569074
Shai Yallin <shai.yallin@gmail.com>
2014-07-19 14:12:19 +0300

    initial commit

---
+++ build.sbt
@@ -0,0 +1,11 @@
+scalaVersion := "2.10.4"
+
+scalacOptions ++= Seq("-unchecked", "-deprecation", "-feature")
+
+name := "logs"
+
+organization := "com.shaiyallin"
+
+version := "0.1.0-SNAPSHOT"
+
+libraryDependencies += "com.netaporter" %% "scala-uri" % "0.4.2"
---
+++ src/main/scala/com/shaiyallin/logs/Logs.scala
@@ -0,0 +1,77 @@
+package com.shaiyallin.logs
+
+import java.time.{ZoneOffset, LocalDateTime}
+import java.time.format.DateTimeFormatter
+import scala.io.Source
+import com.netaporter.uri.Uri
+import com.netaporter.uri.parsing.UriParser
+import com.netaporter.uri.config.UriConfig
+
+/**
+ * @author shaiyallin
+ * @since 7/18/14
+ */
+
+case class Request(ip: String, date: LocalDateTime, method: String, uri: Uri, status: Int, responseSize: Int, userAgent: String)
+
+object Parse {
+  def unapply(uri: String): Option[Uri] = new UriParser(uri, UriConfig.default)._uri.run().toOption
+}
+
+object Request {
+  val NCSALogLine = "^([0-9.]*).*?\\[(\\w.*)?\\]\\s\\\"(\\w*)\\s([\\S]*).*\\\"\\s(\\d*)?\\s(\\d*)?\\s.*\\s\\\"(.*)?\\\".*$".r
+  val dateFormat = DateTimeFormatter.ofPattern("dd/MMM/yyyy:HH:mm:ss Z").withZone(ZoneOffset.UTC)
+
+  def unapply(line: String): Option[Request] = line match {
+    case NCSALogLine(ip, date, method, Parse(uri), status, responseSize, userAgent) =>
+      Some(Request(ip, parseDate(date), method, uri, status.toInt, responseSize.toInt, userAgent))
+    case _ => None
+  }
+
+  val parse: PartialFunction[String, Request] = { case Request(request) => request }
+
+  def parseDate(date: String) = LocalDateTime.parse(date, dateFormat)
+
+
+}
+
+object Logs extends App {
+  type Requests = Seq[Request]
+  val log = Source.fromFile("./request.100th.log")
+  val allRequets = log.getLines().collect(Request.parse).toSeq
+
+  val subnetOf = { ip: String => if (ip.contains(".")) ip.substring(0, ip.lastIndexOf('.')) else ip }
+  val statusIs = { f: (Int => Boolean) => requests: Requests => requests.filter(r => f(r.status))}
+  val popularPaths = { requests: Requests => requests.groupBy(r => r.uri.path).toSeq.sortBy { case (path, rs) => -rs.size} }
+
+  val errors = statusIs(_ >= 400)
+  
+  val errorRateFor = { requests: Requests =>
+    val erroneous = errors(requests)
+    f"Error rate: ${erroneous.size * 100.0 / requests.size}%2.2f%%"
+  }
+
+  val totalCountFor = { requests: Requests => f"Total requests: ${requests.size}%,d" }
+
+  val sizes = { requests: Requests => requests.map(_.responseSize)}
+  val totalSizeFor = { requests: Requests => f"Total response size: ${sizes(requests).sum}%,d bytes" }
+
+  val summary = { rs: Requests => Seq(totalCountFor(rs), totalSizeFor(rs), errorRateFor(rs)) }
+
+  println(summary(allRequets).mkString("\n"))
+
+  val (subnet, requestsFromMostCommonSubnet) = allRequets.groupBy(r => subnetOf(r.ip)).maxBy {case (_, rs) => rs.size}
+  println(f"Most common subnet:\n\t${summary(requestsFromMostCommonSubnet).mkString("\n\t")}")
+
+  println(f"10 Most popular paths:")
+  popularPaths(allRequets).take(10) foreach { case (path, rs) => println(f"$path\n\t${summary(rs).mkString("\n\t")}")}
+
+
+  val mostErroneous = popularPaths(errors(allRequets)).head
+  val errorsByType = mostErroneous._2.groupBy(_.status).map { case (status, rs) => s"${rs.size} requests resulting in status $status"}
+  println(f"Most erroneous path: ${mostErroneous._1}\n\t${errorsByType.mkString("\n\t")}")
+
+  allRequets.groupBy(request => request.date.withSecond(0).withMinute(0)).toSeq.sortBy { case (date, rs) => date.toEpochSecond(ZoneOffset.UTC)}.map {case (date, rs) =>
+    println(s"$date: \n\t${summary(rs).mkString("\n\t")}")
+  }
+}
